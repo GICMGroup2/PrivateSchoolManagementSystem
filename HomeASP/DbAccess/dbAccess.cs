3ebfd6d7965c873c00d9180d744296df686b20bb
KhinMarLarOo <khinmarlaroo11592@gmail.com>
2016-12-16 14:05:10 +0630

    add DbAccess folder

---
+++ HomeASP/DbAccess/dbAccess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
+namespace PSMS.DbAccess
+{
+    class dbAccess
+    {
+        private string path = @"C:\PSMS\ConStrPSMS.txt";
+        private string conStr = "";
+        public SqlConnection conn = new SqlConnection();
+
+        // Reading file path
+        private void getConnectionStr()
+        {
+            StreamReader sr = File.OpenText(path);
+            conStr = sr.ReadLine();
+        }
+
+        //Get connection to Database
+        public SqlConnection Open()
+        {
+            getConnectionStr();
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                return conn;
+            }
+            else
+            {
+                conn = new SqlConnection(conStr);
+                conn.Open();
+            }
+            return conn;
+        }
+
+        //close Database connection
+        public void Close()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+                conn.Close();
+        }
+    
+    }
+}
